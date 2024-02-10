5a63c0cf484dc17ab614f55a26b8fee1f6dffe86
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-02-10 00:05:48 -0300

    feat: add labels and skeleton to room card

--- app/(dashboard)/_components/room-card.tsx
+++ app/(dashboard)/_components/room-card.tsx
@@ -1,13 +1,43 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
 import { Room } from "@/lib/schemas/room";
+import { useAuth } from "@clerk/nextjs";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import Image from "next/image";
 
 export function RoomCard({ room }: { room: Room }) {
+  const { userId } = useAuth();
+  const ownerLabel = room.onwerId === userId ? "Você" : room.onwerId;
+  const lastUpdated = formatDistanceToNow(room._creationTime, {
+    addSuffix: true,
+    locale: ptBR,
+  });
+
   return (
-    <div className="relative flex flex-col bg-white border rounded-lg overflow-hidden hover:shadow-md transition-shadow">
-      <Image src={room.imageUrl} alt={room.name} width={150} height={100} />
-      <span className="py-2 px-4">
-        <h2 className="font-normal">{room.name}</h2>
+    <div className="group relative flex flex-col bg-white border rounded-lg overflow-hidden hover:shadow-md transition-shadow w-[150px]">
+      <Image
+        src={room.imageUrl}
+        alt={room.name}
+        width={150}
+        height={100}
+        className="h-[100px] w-full object-cover"
+      />
+      <span className="px-4 py-2 space-y-1 h-full">
+        <h2 className="truncate">{room.name}</h2>
+        <p className="text-xs truncate text-gray-500 opacity-0 group-hover:opacity-100">
+          {ownerLabel}, {lastUpdated}
+        </p>
       </span>
     </div>
   );
 }
+
+RoomCard.Skeleton = function RoomCardSkeleton() {
+  return (
+    <div className="flex flex-col rounded-lg overflow-hidden w-[150px] h-[160px]">
+      <Skeleton className="w-full h-full" />
+    </div>
+  );
+};
