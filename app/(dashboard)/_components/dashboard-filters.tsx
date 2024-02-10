2c61b838f52f41e5404b3bb4a96f00082771c00a
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-02-10 00:05:21 -0300

    refactor: make filters responsive

--- app/(dashboard)/_components/dashboard-filters.tsx
+++ app/(dashboard)/_components/dashboard-filters.tsx
@@ -5,8 +5,8 @@ import { SearchInput } from "./search-input";
 
 export function DashboardFilters() {
   return (
-    <div className="flex items-center justify-between gap-4">
-      <h2 className="font-semibold text-lg">Minhas Salas</h2>
+    <div className="flex items-center justify-end sm:justify-between gap-4">
+      <h2 className="font-semibold text-2xl hidden sm:block">Salas</h2>
       <div className="inline-flex gap-4">
         <SearchInput />
         <Button
@@ -23,8 +23,8 @@ export function DashboardFilters() {
               },
             }}
           >
-            <Star className="h-4 w-4 mr-2" />
-            Favoritos
+            <Star className="h-4 w-4 " />
+            <span className="hidden sm:block sm:ml-2">Favoritos</span>
           </Link>
         </Button>
       </div>
