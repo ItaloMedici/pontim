95a0330f87cba0e5d891e83e0628196e5e9abc37
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-02-09 22:59:56 -0300

    feat: create convex client provider

---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Loading } from "@/components/auth/loading";
+import { env } from "@/env";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL);
+
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
