309683014118fd0a1076524711f6ae2b43aabacc
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-02-13 02:05:06 -0300

    refactor: add favorite and remove validation

--- lib/schemas/room.ts
+++ lib/schemas/room.ts
@@ -2,10 +2,11 @@ import { z } from "zod";
 
 export const roomValidator = z.object({
   _id: z.string(),
-  name: z.string(),
+  name: z.string().min(4, "Nome da sala deve ter no mínimo 4 caracteres"),
   onwerId: z.string(),
   imageUrl: z.string(),
   _creationTime: z.number(),
+  favorite: z.boolean().optional(),
 });
 
 export type Room = z.infer<typeof roomValidator>;
