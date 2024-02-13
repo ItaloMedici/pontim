26623d5290aa87a101a9da35a357f826c07f76ad
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-02-13 02:04:42 -0300

    refactor: add valitation message

--- lib/schemas/create-room.ts
+++ lib/schemas/create-room.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const createRoomSchema = z.object({
   name: z.string().min(4, "Nome da sala deve ter no mínimo 4 caracteres"),
-  guests: z.array(z.string().email("Email inválido")).optional(),
   onwer: z.string().optional(),
+  imageUrl: z.string().optional(),
 });
 
 export type CreateRoom = z.infer<typeof createRoomSchema>;
