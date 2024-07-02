b640313789ba424ab5c41249dc0998900fb471c7
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-07-02 02:03:57 -0300

    feat: create http handler

--- lib/api/index.tsx
+++ lib/api/index.tsx
@@ -1,5 +1,45 @@
-import * as room from "../../use-cases/room";
+import { toast } from "sonner";
 
-export const api = {
-  room,
+const isServer = typeof window === "undefined";
+
+const createHttpHandler = async <T,>(fn: () => Promise<Response>) => {
+  const result = await fn();
+
+  if (result.ok) {
+    return result.json() as T;
+  }
+
+  const error = await result.json();
+
+  if (isServer || process.env.NODE_ENV === "development") {
+    console.error(error);
+    throw error;
+  }
+
+  const message = "Algo deu errado 😥";
+  toast.error(message);
+
+  return null;
+};
+
+const get = <T,>(url: string) =>
+  createHttpHandler<T>(async () => {
+    return await fetch(`/api/${url}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+  });
+
+const post = <T,>(url: string, body: any) =>
+  createHttpHandler<T>(async () => {
+    return await fetch(`/api/${url}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+  });
+
+export const http = {
+  get,
+  post,
 };
