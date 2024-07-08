eb098ad9f53505cc32c160302393d87df45e5b5b
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-07-07 23:34:37 -0300

    refactor: add variants to numeric card

--- components/card/numeric-card.tsx
+++ components/card/numeric-card.tsx
@@ -1,34 +1,62 @@
 import { cn } from "@/lib/utils";
+import { VariantProps, cva } from "class-variance-authority";
 
-type NumericCardProps = {
-  value: string | number | null;
-  color: string;
+const numericCardVariants = cva(
+  "relative flex items-center justify-center rounded-lg border font-medium font-md",
+  {
+    variants: {
+      color: {
+        primary: "text-sky-500 border-sky-500",
+        gray: "text-gray-500 border-gray-200",
+      },
+      bgColor: {
+        primary: "bg-sky-500 border-sky-500",
+        white: "bg-white",
+        gray: "bg-gray-200 border-gray-200",
+      },
+      size: {
+        default: "w-12 h-16",
+        small: "w-8 h-12",
+        large: "w-16 h-20",
+      },
+      selected: {
+        true: "ring-2 ring-sky-500 shadow-md",
+        flase: "",
+      },
+    },
+    defaultVariants: {
+      color: "gray",
+      size: "default",
+      bgColor: "white",
+    },
+  }
+);
+
+type NumericCardProps = VariantProps<typeof numericCardVariants> & {
   colorOnHover?: boolean;
+  value: number | string | undefined | null;
+  label?: string;
 };
 
 export const NumericCard = ({
   value,
   color,
+  size,
+  selected,
+  bgColor,
+  label,
   colorOnHover,
 }: NumericCardProps) => {
   return (
-    <div
-      className={cn(
-        `relative flex items-center justify-center w-full h-full rounded-lg border-gray-300`,
-        {
-          [`bg-${color}/30 text-${color}`]: !colorOnHover,
-          [`bg-gray-100 text-gray-600 hover:bg-${color}/30 hover:text-${color}`]:
-            colorOnHover,
-        }
-      )}
-    >
-      <div className="absolute top-0 left-0 text-xs flex items-center w-3 aspect-square bg-white rounded-full">
-        <span className="opacity-50">{value}</span>
+    <div className="flex flex-col gap-2 items-center">
+      <div
+        className={cn(numericCardVariants({ color, size, selected, bgColor }))}
+      >
+        <div>{value}</div>
       </div>
-      <div>{value}</div>
-      <div className="absolute bottom-0 right-0 text-xs flex items-center w-3 aspect-square bg-white rounded-full">
-        <span className="opacity-50">{value}</span>
-      </div>
+      {label ? (
+        <span className="text-sm font-medium text-gray-700">{label}</span>
+      ) : null}
     </div>
   );
 };
