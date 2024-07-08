25b3448953a66846be58865b6ba502a1b30856ee
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-07-07 23:35:25 -0300

    refactor: add prisma to global

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import { gl } from "date-fns/locale";
 
-const prisma = new PrismaClient();
+const prisma =
+  ((globalThis as any).prisma as PrismaClient) ?? new PrismaClient();
+
+if (!(globalThis as any).prisma) (globalThis as any).prisma = prisma;
 
 export { prisma as db };
