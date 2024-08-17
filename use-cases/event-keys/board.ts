0fbc20bafac4d1e8a3a16f0e3aa84e0ccc3c461f
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-08-17 07:39:29 -0300

    break: remove socket

--- use-cases/event-keys/board.ts
+++
@@ -1,9 +0,0 @@
-const createBoardEventKey = (event: string) => {
-  return (boardId: string) => `board:${boardId}:${event}`;
-};
-
-export const joinBoardKey = createBoardEventKey("join");
-export const leaveBoardKey = createBoardEventKey("leave");
-export const playerChoiceKey = createBoardEventKey("choice");
-export const revealCardsKey = createBoardEventKey("reveal");
-export const resetBoardKey = createBoardEventKey("reset");
