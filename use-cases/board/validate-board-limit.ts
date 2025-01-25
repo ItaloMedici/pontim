87d9e7c79ee6142d67b9bd37415b09bf4c8ae03e
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2025-01-25 07:20:20 -0300

    feat: extract validation from join board

--- use-cases/board/validate-board-limit.ts
+++ use-cases/board/validate-board-limit.ts
@@ -3,15 +3,13 @@ import { z } from "zod";
 import { getPlayersByBoardId } from "../player/get-players-by-board-id";
 import { validator } from "../validator";
 
-export const validateBoardLimit = validator({
+export const canAddMorePlayersBoard = validator({
   input: z.object({
     boardId: z.string().uuid(),
   }),
   handler: async ({ boardId }) => {
     const currentPlayers = await getPlayersByBoardId({ boardId: boardId });
 
-    if (currentPlayers.length >= Number(env.MAXIMUM_PLAYERS_PER_BOARD)) {
-      throw new Error("Board is full");
-    }
+    return currentPlayers.length < Number(env.MAXIMUM_PLAYERS_PER_BOARD);
   },
 });
