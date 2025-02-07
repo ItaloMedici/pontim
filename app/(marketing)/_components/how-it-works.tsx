39b6455a8ec4f89cc9c440c2a6428453b26b448a
Jefferson Gerson dos Santos <jefferson.santos@maxmilhas.com.br>
2025-02-06 23:04:04 -0300

    refactor: adjust visible viewport animation

--- app/(marketing)/_components/how-it-works.tsx
+++ app/(marketing)/_components/how-it-works.tsx
@@ -62,7 +62,7 @@ export const HowItWorks = () => {
         className="mx-auto"
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: true, margin: "-100px" }}
+        viewport={{ once: true, margin: "-100px", amount: 0.2 }}
         variants={containerVariants}
       >
         <motion.h2
