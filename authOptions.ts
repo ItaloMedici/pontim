ee8b5b943897eca147f1f63f66ddc922d24091de
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2025-02-08 02:46:23 -0300

    feat: add plan name on jwt

--- authOptions.ts
+++ authOptions.ts
@@ -4,7 +4,7 @@ import { createUser } from "@/use-cases/user/create-user";
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { User } from "./lib/schemas/user";
-import { getUserByEmail } from "./use-cases/user/get-user-by-email";
+import { getSubscriptionByUser } from "./use-cases/subscription/get-subscription-by-user";
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -25,26 +25,30 @@ export const authOptions: AuthOptions = {
 
       if (!token.email) return token;
 
-      let user: User | undefined | null = await getUserByEmail(token.email);
+      let user: User | undefined | null = await createUser({
+        email: token.email,
+        name: token.name as string,
+        image: token.picture as string,
+      });
 
-      if (!user && trigger === "signIn") {
-        user = await createUser({
-          email: token.email,
-          name: token.name as string,
-          image: token.picture as string,
-        });
+      let planName: string | undefined;
 
-        if (user) {
-          createCustomer(user);
-        }
+      if (user) {
+        createCustomer(user);
+
+        const subscription = await getSubscriptionByUser({ userId: user.id });
+
+        planName = subscription?.plan.name;
       }
 
       token.id = user?.id;
+      token.planName = planName;
       return token;
     },
     async session({ session, token }) {
       (session as any).accessToken = token.accessToken;
       (session as any).user.id = token.id;
+      (session as any).user.planName = token.planName;
 
       return session;
     },
