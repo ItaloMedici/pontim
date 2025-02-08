929602a9e1de3872695317f4aadb443a480af2d7
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2025-02-08 02:45:20 -0300

    fix(webhook): rename route

--- app/api/(payments)/stripe/webhook/route.ts
+++ app/api/(payments)/stripe/webhook/route.ts
@@ -2,12 +2,17 @@ import Stripe from "stripe";
 
 import { env } from "@/env";
 import stripe from "@/lib/stripe";
+import { processDeleteCustomer } from "@/use-cases/stripe/process-delete-customer";
+import { processDeleteSubscription } from "@/use-cases/stripe/process-delete-subscription";
 import { processUpdateSubscription } from "@/use-cases/stripe/process-update-subscription";
 import { headers } from "next/headers";
 
 export async function POST(req: Request) {
+  console.log("Received webhook");
   const body = await req.text();
   const signature = headers().get("Stripe-Signature") as string;
+
+  console.log(`Received webhook with signature ${signature}`);
 
   let event: Stripe.Event;
 
@@ -22,10 +27,18 @@ export async function POST(req: Request) {
     return new Response(`Webhook Error: ${error.message}`, { status: 400 });
   }
 
+  console.log(`Received event: ${event.type}`);
+
   switch (event.type) {
     case "customer.subscription.created":
     case "customer.subscription.updated":
       await processUpdateSubscription(event.data);
+      break;
+    case "customer.subscription.deleted":
+      processDeleteSubscription(event.data);
+      break;
+    case "customer.deleted":
+      processDeleteCustomer(event.data);
       break;
     default:
       console.log(`Unhandled event type ${event.type}`);
