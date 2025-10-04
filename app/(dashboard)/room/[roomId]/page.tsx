849212133d789539040b0d72f014551d18bc29af
Ítalo <italomedice969@gmail.com>
2025-10-04 16:46:24 -0300

    feat(instant-room): ensure combined session in room page

--- app/(dashboard)/room/[roomId]/page.tsx
+++ app/(dashboard)/room/[roomId]/page.tsx
@@ -1,10 +1,14 @@
+import { PlayerGuestLogin } from "@/components/player-guest-login";
 import { ReportButton } from "@/components/report-button";
 import { SupportButton } from "@/components/support-button";
+import { BoardProvider } from "@/context/board";
+import { getCombinedSession } from "@/lib/auth/universal-auth";
 import { getRoom } from "@/use-cases/room";
 import { Metadata } from "next";
+import { notFound, redirect } from "next/navigation";
+import { cache } from "react";
 import { Board } from "./_components/board";
 import { BoardAds } from "./_components/board-ads";
-import { Room } from "./_components/room";
 
 interface RoomIdPageProps {
   params: {
@@ -12,10 +16,12 @@ interface RoomIdPageProps {
   };
 }
 
+const chachedRoom = cache((roomId: string) => getRoom({ roomId }));
+
 export const generateMetadata = async ({
   params: { roomId },
 }: RoomIdPageProps): Promise<Metadata> => {
-  const room = await getRoom({ roomId });
+  const room = await chachedRoom(roomId);
 
   const roomName = room.name;
 
@@ -25,13 +31,28 @@ export const generateMetadata = async ({
 };
 
 async function RoomPage({ params: { roomId } }: RoomIdPageProps) {
+  const room = await chachedRoom(roomId);
+  const session = await getCombinedSession();
+
+  if (!room || room.isExpired) {
+    notFound();
+  }
+
+  if (room.isTemporary && !session) {
+    return <PlayerGuestLogin roomName={room.name} />;
+  }
+
+  if (!session) {
+    redirect(`/login?callbackUrl=/room/${roomId}`);
+  }
+
   return (
-    <Room roomId={roomId}>
+    <BoardProvider roomId={roomId}>
       <ReportButton />
       <SupportButton />
       <BoardAds />
       <Board />
-    </Room>
+    </BoardProvider>
   );
 }
 
